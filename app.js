aaaf909e553fc6578b0b272c383fd3c8d0134901
Sushant <sushantlamsal31@gmail.com>
2022-06-04 13:09:28 +0545

    ignored node modules

---
+++ app.js
@@ -0,0 +1,43 @@
+//making const because its value should'nt be changed 
+// const express = require('express')
+// const app = express()
+// //require to load router.js file into main app
+// const router= require('./router')
+
+// //method in express to recognize the incoming request object as strings or arrays
+// app.use(express.urlencoded({extended:false}))
+
+// app.use(express.json())
+
+// //making public folder accessible and express.static is a method
+// app.use(express.static('public'))
+
+// //views is a config option and another viewes is a folder
+// app.set('views', 'views')
+
+
+// //let express know which template system we are using and ejs is the template engine
+// app.set('view engine', 'ejs')
+
+
+// //tell express application to use that router
+// app.use('/', router)
+
+
+// module.exports = app
+
+const express = require('express')
+const app = express()
+
+const router = require('./router')
+
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
+
+app.use(express.static('public'))
+app.set('views', 'views')
+app.set('view engine', 'ejs')
+
+app.use('/', router)
+
+module.exports = app
---
+++ router.js
@@ -0,0 +1,13 @@
+//module.exports tells node js which bits of code to export from a given file so other files are allowed to access the exported code
+//router is just a route that routes to a specific file
+
+const express = require('express')
+const router = express.Router()
+const userController = require('./controllers/userController')
+
+router.get('/', userController.home)
+router.post('/register', userController.register)
+
+router.post('/login', userController.login)
+
+module.exports = router
