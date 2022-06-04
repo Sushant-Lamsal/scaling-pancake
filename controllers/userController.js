bceacf8bad74c9dd14406fc20a70b2f0068c34c7
Sushant <sushantlamsal31@gmail.com>
2022-06-04 20:03:33 +0545

    added profile picture photo for the user

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,7 +4,7 @@ const User = require('../models/User')
 exports.login = function(req, res) {
     let user = new User(req.body)
     user.login().then(function(result){
-        req.session.user = {
+        req.session.user = {avatar: user.avatar,
             username: user.data.username}
             req.session.save(function(){
                 res.redirect('/')
@@ -29,7 +29,7 @@ exports.register = function(req, res) {
 let user = new User(req.body)
     user.register().then(()=>{
         req.session.user = {
-            username: user.data.username
+            username: user.data.username, avatar: user.avatar
         }
         req.session.save(function(){
             res.redirect('/')
@@ -47,7 +47,7 @@ let user = new User(req.body)
 exports.home = function(req, res) {
     if(req.session.user){
         res.render('home-dashboard' , {
-            username: req.session.user.username
+            username: req.session.user.username, avatar: req.session.user.avatar
         })
     }else{
         res.render('home-guest' , {errors: req.flash('errors'), regErrors: req.flash('regErrors')} )
